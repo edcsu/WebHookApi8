8163cfddc6f78b036f04071a1793291e763463b5
Ssewannonda Keith <38857594+edcsu@users.noreply.github.com>
2023-12-14 22:26:39 +0300

    feat: initial migration

---
+++ WebHookApi/Migrations/20231214191815_init.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebHookApi.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ActorId = table.Column<Guid>(type: "uuid", nullable: true),
+                    TimeStamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    EventType = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WebHooks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    WebHookUrl = table.Column<string>(type: "text", nullable: false),
+                    Secret = table.Column<string>(type: "text", nullable: true),
+                    ContentType = table.Column<string>(type: "text", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    HookEvents = table.Column<int[]>(type: "integer[]", nullable: false),
+                    LastTrigger = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebHooks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WebHookCreatedEvent",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    WebHookId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebHookCreatedEvent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WebHookCreatedEvent_Events_Id",
+                        column: x => x.Id,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WebHookRemovedEvent",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    WebHookId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebHookRemovedEvent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WebHookRemovedEvent_Events_Id",
+                        column: x => x.Id,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WebHookUpdatedEvent",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    WebHookId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebHookUpdatedEvent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WebHookUpdatedEvent_Events_Id",
+                        column: x => x.Id,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WebHookHeader",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    WebHookId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false),
+                    CreatedTimestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebHookHeader", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WebHookHeader_WebHooks_WebHookId",
+                        column: x => x.WebHookId,
+                        principalTable: "WebHooks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WebHooksHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    WebHookId = table.Column<Guid>(type: "uuid", nullable: false),
+                    HookType = table.Column<int>(type: "integer", nullable: false),
+                    Result = table.Column<int>(type: "integer", nullable: false),
+                    StatusCode = table.Column<int>(type: "integer", nullable: false),
+                    ResponseBody = table.Column<string>(type: "text", nullable: false),
+                    RequestBody = table.Column<string>(type: "text", nullable: false),
+                    RequestHeaders = table.Column<string>(type: "text", nullable: false),
+                    Exception = table.Column<string>(type: "text", nullable: false),
+                    Timestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebHooksHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WebHooksHistory_WebHooks_WebHookId",
+                        column: x => x.WebHookId,
+                        principalTable: "WebHooks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "WebHooks",
+                columns: new[] { "Id", "ContentType", "HookEvents", "IsActive", "LastTrigger", "Secret", "WebHookUrl" },
+                values: new object[] { new Guid("13d97a50-02d7-48cb-b215-fc9c5d9cb638"), "application/json", new[] { 0 }, true, null, null, "https://localhost:5015/hookloopback" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebHookHeader_WebHookId",
+                table: "WebHookHeader",
+                column: "WebHookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebHooks_Id",
+                table: "WebHooks",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebHooksHistory_WebHookId",
+                table: "WebHooksHistory",
+                column: "WebHookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebHookCreatedEvent");
+
+            migrationBuilder.DropTable(
+                name: "WebHookHeader");
+
+            migrationBuilder.DropTable(
+                name: "WebHookRemovedEvent");
+
+            migrationBuilder.DropTable(
+                name: "WebHookUpdatedEvent");
+
+            migrationBuilder.DropTable(
+                name: "WebHooksHistory");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "WebHooks");
+        }
+    }
+}
--- WebHookApi/Program.cs
+++ WebHookApi/Program.cs
@@ -10,38 +10,56 @@ namespace WebHookApi
         {
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "";
 
-            var builder = WebApplication.CreateBuilder(args);
-            var configuration = builder.Configuration;
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddApiDbContext(configuration, environment);
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                var builder = WebApplication.CreateBuilder(args);
+                var configuration = builder.Configuration;
+
+                // Add services to the container.
+
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                builder.Services.AddApiDbContext(configuration, environment);
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                app.UseFileServer();
+
+                ServiceExtension.Initialize(app);
+
+                app.MapControllers();
+
+                app.Run();
             }
+            catch (Exception ex)
+            {
+                var type = ex.GetType().Name;
+                if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+                {
+                    throw;
+                }
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.UseFileServer();
-
-            ServiceExtension.Initialize(app);
-
-            app.MapControllers();
-
-            app.Run();
+                //Log.Fatal(ex, "Unhandled exception");
+            }
+            finally
+            {
+                //Log.Information("Shut down complete");
+                //Log.CloseAndFlush();
+            }
         }
     }
 }
